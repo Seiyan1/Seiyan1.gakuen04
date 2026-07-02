743077820d8105d54d526d5010913b206245bcac
Seiyan1 <seiya_ogino@mail.toyota.co.jp>
2026-07-02 14:54:10 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict";
+// 1行目に記載している "use strict" は削除しないでください
+
+/*document.getElementById("timetable")は
+HTMLの中から idが「timetable」の要素（ボタン）を探すという命令*/
+
+/*.onclick=>そのボタンがクリックされたら右の関数が実行されるよ*/
+
+document.getElementById("timetable").onclick = function () {
+    window.location.href = "../ジプリ 時間割/index.html";
+};
+
+document.getElementById("belongings").onclick = function () {
+    window.location.href = "../ジプリ テスト順位/index.html";
+};
+
+document.getElementById("test").onclick = function () {
+    window.location.href = "../ジプリ テスト範囲/index.html";
+};
